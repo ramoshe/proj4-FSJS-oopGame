e7ddedc2d72c8900d88e84c7470e8ae64757fcc0
ramoshe <rsherard@mac.com>
2021-07-05 16:29:40 -0400

    Phrase and Game classes built

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,90 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+    constructor() {
+        this.missed = 0;
+        this.phrases = ['live in the moment', 
+                        'follow your heart',
+                        'positive vibes only',
+                        'you are enough',
+                        'life is a journey'];
+        this.activePhrase = null;
+    }
+
+    /**
+     * Initializes game
+     */
+    startGame() {
+        document.querySelector('#overlay').style.display = 'none';
+        this.activePhrase = this.phrases.getRandomPhrase();
+        this.activePhrase.addPhraseToDisplay();
+    }
+
+    /**
+     * Randomly retrieves one of the phrases
+     */
+    getRandomPhrase() {
+        return this.phrases[Math.floor(Math.random() * this.phrases.length)];
+    }
+
+    /**
+     * Controls most of the game logic
+     */
+    handleInteraction(event) {
+        const clickedLetter = event.target.textContent;
+        event.target.disabled = true;
+        if (!this.activePhrase.checkLetter(clickedLetter)) {
+            event.target.classList.add('wrong');
+            this.removeLife();
+        } else {
+            event.target.classList.add('chosen');
+            this.activePhrase.showMatchedLetter(clickedLetter);
+            if (this.checkforWin()) {
+                this.gameOver();
+            }
+        }
+    }
+
+    /**
+     * Removes a life from the scoreboard
+     */
+    removeLife() {
+        const liveHeart = document.querySelector('[src="images/liveHeart.png"]');
+        liveHeart.setAttribute('src', 'images/lostHeart.png');
+        this.missed += 1;
+        if (this.missed === 5) {
+            this.gameOver();
+        }
+    }
+
+    /**
+     * Checks if player has revealed all letters
+     */
+    checkforWin() {
+        const allRevealed = false;
+        const hiddenLetter = document.querySelector('.hide');
+        if (hiddenLetter === null) {
+            allRevealed = true;
+        }
+        return allRevealed;
+    }
+
+    /**
+     * Displays overlay with win or loss message
+     */
+    gameOver() {
+        const overlay = document.querySelector('#overlay');
+        overlay.style.display = 'flex';
+        overlay.classList.remove('start');
+        const message = document.querySelector('#game-over-message');
+        if (this.checkforWin()) {
+            message.textContent = 'Congrats! You won the game!';
+            overlay.classList.add('win');
+        } else {
+            message.textContent = 'Sorry, you lost the game.';
+            overlay.classList.add('lose');
+        }
+    }
+}
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,55 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+class Phrase {
+    constructor(phrase) {
+        this.phrase = phrase.toLowerCase();
+    }
+
+    /**
+     * Adds letter placeholders for game start
+     */
+    addPhraseToDisplay() {
+        const phraseContainer = document.querySelector('#phrase ul');
+        const phraseArray = this.phrase.split('');
+        phraseArray.forEach(character => {
+            if (character === ' ') {
+                const space = document.createElement('li');
+                space.className = 'space';
+                space.textContent = character;
+                phraseContainer.appendChild(space);
+            } else {
+                const letter = document.createElement('li');
+                letter.classList.add('hide', 'letter', character);
+                letter.textContent = character;
+                phraseContainer.appendChild(letter);
+            }
+        });
+    }
+
+    /**
+     * Checks to see if player's selected letter is in phrase
+     */
+    checkLetter(guess) {
+        const guessIsCorrect = false;
+        const phraseArray = this.phrase.split('');
+        phraseArray.forEach(character => {
+            if (guess == character) {
+                guessIsCorrect = true;
+            }
+        });
+        return guessIsCorrect;
+    }
+
+    /**
+     * Reveals the letter(s) on board that match player's selection
+     */
+    showMatchedLetter(letter) {
+        const matchingLetters = document.getElementsByClassName(letter);
+        matchingLetters.forEach(letter => {
+            letter.classList.add('show');
+            letter.classList.remove('hide');
+        });
+    }
+}
